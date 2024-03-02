ee02dd50cf878d3f8c5a734b5748a41e34f83f13
terrencekliu <terrencekliu@comcast.net>
2024-03-02 14:07:09 -0800

    Add supplier classes
    Includes supplier, supplyContract, ingredient, transaction

---
+++ src/main/java/csc366/jpademo/supplier/Ingredient.java
@@ -0,0 +1,94 @@
+package csc366.jpademo.supplier;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.StringJoiner;
+
+@Entity  // indicates that this class maps to a database table
+@Table(
+        name = "ingredient"     // may be omitted for default table naming
+)
+public class Ingredient {
+    @Id
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
+    private Long id;
+
+    @NotNull
+    private String name;
+
+    @NotNull
+    @Column(name = "num_unit")
+    private int numUnit;
+
+    private String description;
+
+    @OneToMany(
+            targetEntity = Transaction.class,
+            mappedBy = "transaction",
+            cascade = CascadeType.PERSIST,
+            fetch = FetchType.LAZY,
+            orphanRemoval = true
+    )
+    private List<Transaction> transactions = new ArrayList<>();
+
+    public Ingredient() {}
+
+    public Ingredient(String name, int numUnit, String description) {
+        this.name = name;
+        this.numUnit = numUnit;
+        this.description = description;
+    }
+
+    public Long getId() {
+        return id;
+    }
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public int getNumUnit() {
+        return this.numUnit;
+    }
+
+    public void setNumUnit(int numUnit) {
+        this.numUnit = numUnit;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public List<Transaction> getTransactions() {
+        return transactions;
+    }
+
+    public void setTransactions(List<Transaction> transactions) {
+        this.transactions = transactions;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    @Override
+    public String toString() {
+        StringJoiner sj = new StringJoiner("," , Ingredient.class.getSimpleName() + "[" , "]");
+        sj.add(id.toString()).add(name).add(String.valueOf(numUnit)).add(description).add("transactions="+transactions.toString());
+        return sj.toString();
+    }
+
+    @Override
+    public int hashCode() {
+        return 366;
+    }
+}
---
+++ src/main/java/csc366/jpademo/supplier/Transaction.java
@@ -0,0 +1,132 @@
+package csc366.jpademo.supplier;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.StringJoiner;
+
+@Entity  // indicates that this class maps to a database table
+@Table(
+        name = "transaction"     // may be omitted for default table naming
+)
+public class Transaction {
+    @Id
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
+    private Long id;
+
+    @NotNull
+    @Column(name="num_case")
+    private int numCase;
+
+    @Column
+    private String notes;
+
+    @OneToOne(
+            targetEntity = SupplyContract.class,
+            mappedBy = "supply_contract",
+            cascade = CascadeType.REMOVE,
+            fetch = FetchType.LAZY,
+            optional = false
+    )
+    private SupplyContract supplyContract;
+
+    @ManyToOne(
+            targetEntity = Supplier.class,
+            cascade = CascadeType.REMOVE,
+            fetch = FetchType.LAZY
+    )
+    private Supplier supplier;
+
+    @OneToMany(
+            targetEntity = Ingredient.class,
+            mappedBy = "ingredient",
+            cascade = CascadeType.PERSIST,
+            fetch = FetchType.LAZY,
+            orphanRemoval = true
+    )
+    private List<Ingredient> ingredients = new ArrayList<>();
+
+    // TODO: Link Transation to DrinkVariation table
+//    @ManyToOne(
+//            targetEntity = DrinkVariation.class,
+//            cascade = CascadeType.REMOVE,
+//            fetch = FetchType.LAZY,
+//            optional = false
+//    )
+//    private DrinkVariation drinkVariation;
+
+    public Transaction() { }
+
+    public Transaction(int numCase, String notes) {
+        this.numCase = numCase;
+        this.notes = notes;
+    }
+
+    public Long getId() {
+        return id;
+    }
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public int getNumCase() {
+        return this.numCase;
+    }
+    public void setNumCase(int numCase) {
+        this.numCase = numCase;
+    }
+
+    public String getNotes() {
+        return notes;
+    }
+    public void setNotes(String notes) {
+        this.notes = notes;
+    }
+
+    public void setSupplier(Supplier supplier) {
+        this.supplier = supplier;
+    }
+
+    public Supplier getSupplier() {
+        return supplier;
+    }
+
+    public void setSupplyContract(SupplyContract supplyContract) {
+        this.supplyContract = supplyContract;
+    }
+
+    public SupplyContract getSupplyContract() {
+        return supplyContract;
+    }
+
+    public List<Ingredient> getIngredients() {
+        return ingredients;
+    }
+
+    public void setIngredients(List<Ingredient> ingredients) {
+        this.ingredients = ingredients;
+    }
+
+    // TODO: Link Transaction to DrinkVariation table
+//    public DrinkVariation getDrinkVariation() {
+//        return drinkVariation;
+//    }
+//
+//    public void setDrinkVariation(DrinkVariation dv) {
+//        this.drinkVariation = dv;
+//    }
+
+    @Override
+    public String toString() {
+        // TODO: Add new relations here
+        StringJoiner sj = new StringJoiner("," , SupplyContract.class.getSimpleName() + "[" , "]");
+        sj.add(id.toString()).add(String.valueOf(numCase)).add(notes).add(supplyContract.toString()).add(supplier.toString()).add("ingredients="+ingredients.toString());
+        return sj.toString();
+    }
+
+    @Override
+    public int hashCode() {
+        return 366;
+    }
+}
